b57b4d3d2e9c1b6347bdb9ed5beb16ae38241e73
EoAndrelz <raq.andre101810@gmail.com>
2022-09-14 11:25:50 -0300

    doing the basic of the server

--- teste.js
+++ teste.js
@@ -2,7 +2,19 @@ const express = require("express")
 
 const app=express()
 
+app.get("/", function(req,resp){
+    resp.send("Bem vindo a seu app");
+
+})
+
+app.get("/contato",function(req,resp){
+    resp.send("Pagina de contato do seu app");
+})
+    
+app.get("/produto", function(req,resp){
+        resp.send("Pagina de Produto do seu app");
+    })
 
 app.listen(8081,function(){
     console.log("O Servidor esta Funcionando na url http://localhost:8081");
-})
+});
